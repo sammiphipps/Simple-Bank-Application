7d29d1f605efa506ea424b32facaf699c0e3f9ac
Samantha Phipps <sammi.phipps@gmail.com>
2023-01-17 22:10:00 -0500

    Get name and customer ID; Produce menu options

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,4 +1,28 @@
 
 public class BankAccount {
 
+	private String customerName; 
+	private int customerId; 
+	
+	BankAccount(String customerName, int customerId){
+		this.customerName = customerName; 
+		this.customerId = customerId; 
+	}
+	
+	public void showMenu() {
+		System.out.println(); 
+		System.out.println("************************************************************"); 
+		System.out.println(); 
+		System.out.println("Welcome " + this.customerName); 
+		System.out.println("Your Id is " + this.customerId); 
+		System.out.println("\n"); 
+		System.out.println("Available Menu Options: "); 
+		System.out.println("1. Check Balance"); 
+		System.out.println("2. Deposit"); 
+		System.out.println("3. Withdrawl"); 
+		System.out.println("4. View Previous Transaction"); 
+		System.out.println("5. Exit"); 
+		System.out.println();
+		System.out.println("************************************************************"); 
+	}
 }
--- src/BankApplication.java
+++ src/BankApplication.java
@@ -4,7 +4,24 @@ public class BankApplication {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in); 
+		
+		//Request customer Name and customerId
+		System.out.print("Please provide name: "); 
+		String customerName = sc.next();
+		sc.nextLine();
+		
+		System.out.print("What is your customerId: "); 
+		int customerId = sc.nextInt(); 
+		sc.nextLine(); 
+		
+		sc.close(); 
+		
+		//Initialize Bank Account 
+		BankAccount account = new BankAccount(customerName, customerId); 
+		
+		account.showMenu();
+		
 	}
 
 }
